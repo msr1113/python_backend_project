f39cb90c796409f9b7eabf180ff44f4e8a40bfaa
mrawal <imsr1013@gmail.com>
2022-07-15 15:28:52 +0545

    input text in form page and count words in text in counter page

--- myapp/views.py
+++ myapp/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    context ={
-        'name':'Patrick',
-        'age':23,
-        'nationality':'British'
-    }
-    return render(request,'index.html',context)
+    return render(request,'index.html')
+
+def counter(request):
+    text = request.GET['text']
+    amount_of_words = len(text.split())
+    return render(request,'counter.html',{'amount':amount_of_words})
 
